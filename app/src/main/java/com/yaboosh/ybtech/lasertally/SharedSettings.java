e7b894c15f0f85160eccf09019907dbf6894da61
Hunter Schoonover <hunter@skoonie.com>
2015-02-17 13:57:08 -0700

    SharedSettings class now implements parcelable.
    This allows any instance of the class to be passed between activities
    with intent extras.
    
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- app/src/main/java/com/yaboosh/ybtech/lasertally/SharedSettings.java
+++ app/src/main/java/com/yaboosh/ybtech/lasertally/SharedSettings.java
@@ -7,6 +7,19 @@
  *
  * This class contains variables shared among various objects.
  *
+ * Implements Parcelable so that an instance of this class can be passed from
+ * one activity to another using intent extras.
+ *
+ * For each non-static variable that is added to the class:
+ *      search for !!STORE VARIABLES IN PARCEL HERE!! and add the variable
+ *          to the parcel, using the others as examples
+ *
+ *      search for !!GET VARIABLES FROM PARCEL HERE!! and get the variable
+ *          from the parcel, using the others as examples
+ *
+ * IMPORTANT: The orders the variables are stored and retrieved from the
+ *              parcel must match!
+ *
  */
 
 //-----------------------------------------------------------------------------
@@ -20,11 +33,17 @@ package com.yaboosh.ybtech.lasertally;
 
 import android.content.Context;
 import android.os.Environment;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
 
 import java.io.File;
 
-public class SharedSettings {
+public class SharedSettings implements Parcelable {
+
+    private static final String LOG_TAG = "SharedSettings";
+
+    public static Parcelable.Creator CREATOR;
 
     private Context context;
     // Getter and setter functions
@@ -46,7 +65,29 @@ public class SharedSettings {
     public String getJobsFolderPath() { return jobsFolderPath; }
     public void setJobsFolderPath(String pNewPath) { jobsFolderPath = pNewPath; }
 
-    private static final String LOG_TAG = "SharedSettings";
+    //-----------------------------------------------------------------------------
+    // SharedSettings::SharedSettings (constructor)
+    //
+    // Constructor to be used for initial creation.
+    //
+
+    public SharedSettings() {
+
+    }//end of SharedSettings::SharedSettings (constructor)
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    // SharedSettings::SharedSettings (constructor)
+    //
+    // Constructor to be used when creating the object from a parcel.
+    //
+
+    public SharedSettings(Parcel pIn) {
+
+        readFromParcel(pIn);
+
+    }//end of SharedSettings::SharedSettings (constructor)
+    //-----------------------------------------------------------------------------
 
     //-----------------------------------------------------------------------------
     // SharedSettings::init
@@ -55,6 +96,8 @@ public class SharedSettings {
     //
 
     public void init() {
+
+        initializeCreatorVariable();
 
         preparesDataStoragePath("Tally Zap");
 
@@ -129,6 +172,91 @@ public class SharedSettings {
         reportsFolderPath = file.toString() + File.separator;
 
     }// end of SharedSettings::prepareReportsFolderPath
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    // SharedSettings::readFromParcel
+    //
+    // Reads and stores the variables from the passed in parcel in the same order
+    // that the variables were written to the parcel.
+    //
+    // Called from the constructor to create this object from a parcel.
+    //
+
+    private void readFromParcel(Parcel pParcel) {
+
+        //!!STORE VARIABLES IN PARCEL HERE!!
+        dataFolderPath = pParcel.readString();
+        reportsFolderPath = pParcel.readString();
+        jobsFolderPath = pParcel.readString();
+
+    }// end of SharedSettings::readFromParcel
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    // SharedSettings::describeContents
+    //
+    // Required to override because of Parcelable
+    //
+
+    @Override
+    public int describeContents() {
+
+        return 0;
+
+    }// end of SharedSettings::describeContents
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    // SharedSettings::writeToParcel
+    //
+    // Writes each variable into the parcel.
+    //
+    // Context is not written to the parcel because its value will be set after
+    // the SharedSettings object is given to another activity, so preserving its
+    // value is not important.
+    //
+
+    @Override
+    public void writeToParcel(Parcel pParcel, int pFlags) {
+
+        //!!GET VARIABLES FROM PARCEL HERE!!
+        pParcel.writeString(dataFolderPath);
+        pParcel.writeString(reportsFolderPath);
+        pParcel.writeString(jobsFolderPath);
+
+    }// end of SharedSettings::writeToParcel
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    // SharedSettings::initializeCreatorVariable
+    //
+    // Initializes the CREATOR variable, overriding class functions as necessary.
+    //
+    // "The Parcelable.Creator interface must be implemented and provided as a
+    // public CREATOR field that generates instances of your Parcelable class
+    // from a Parcel." This function does just that.
+    //
+
+    private void initializeCreatorVariable() {
+
+        CREATOR = new Parcelable.Creator() {
+
+            //Create a new instance of the SharedSettings class,
+            //instantiating it from the given Parcel
+            @Override
+            public SharedSettings createFromParcel(Parcel pParcel) {
+                return new SharedSettings(pParcel);
+            }
+
+            //Create a new array of the SharedSettings class
+            @Override
+            public SharedSettings[] newArray(int pSize) {
+                return new SharedSettings[pSize];
+            }
+        };
+
+    }//end of SharedSettings::initializeCreatorVariable
     //-----------------------------------------------------------------------------
 
 }//end of class SharedSettings
