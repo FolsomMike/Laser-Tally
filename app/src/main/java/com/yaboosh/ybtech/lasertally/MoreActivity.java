10aafda5d7a213f44292ac06256f241f8e092642
Hunter Schoonover <hunter@skoonie.com>
2015-02-18 20:45:25 -0700

    Changed the name of the xml file tied to the more activity.
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- app/src/main/java/com/yaboosh/ybtech/lasertally/MoreActivity.java
+++ app/src/main/java/com/yaboosh/ybtech/lasertally/MoreActivity.java
@@ -20,14 +20,10 @@ package com.yaboosh.ybtech.lasertally;
 //-----------------------------------------------------------------------------
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
-
-import java.io.File;
 
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -68,7 +64,7 @@ public class MoreActivity extends Activity {
 
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_more);
 
         this.setFinishOnTouchOutside(false);
 
