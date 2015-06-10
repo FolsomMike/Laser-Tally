e13c3e9f26d23507295b1494dda6036db4c7679c
Hunter Schoonover <hunter@skoonie.com>
2015-06-10 14:27:48 -0600

    Quick Job names are now the date and time of creation.
    Signed-off-by: Hunter Schoonover <hunter@skoonie.com>

--- app/src/main/java/com/yaboosh/ybtech/lasertally/JobsHandler.java
+++ app/src/main/java/com/yaboosh/ybtech/lasertally/JobsHandler.java
@@ -47,6 +47,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -266,13 +267,7 @@ public class JobsHandler implements Parcelable {
     public void createQuickJob()
     {
 
-        //WIP HSS// -- need to get date
-
-        String name = "";
-        int i = 1;
-        do { name = "job" + i++; }  while (checkIfJobNameAlreadyExists(name));
-
-        jobName = name;
+        jobName = date + " ~ " + DateFormat.getTimeInstance().format(new Date());
 
         setFilePaths(jobName);
 
